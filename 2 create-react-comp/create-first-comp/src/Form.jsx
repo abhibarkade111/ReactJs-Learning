9ec69de163e93a678f0752d4eedc1a75d6897c42
Abhishek Barkade <abhibarkade111@gmail.com>
2024-02-20 23:21:48 +0530

    Added new code

---
+++ 2 create-react-comp/create-first-comp/src/Form.jsx
@@ -0,0 +1,11 @@
+function Form(){
+  return <form action="submit">
+    <input type="text" name="Name"/>
+    <input type="text" name="City"/>
+    <input type="text" name="Designation"/>
+    <input type="text" name="Exprience"/>
+    <button>Click Me</button>
+  </form>
+}
+
+export default Form;
---
+++ 2 create-react-comp/create-first-comp/src/MyButton.jsx
@@ -0,0 +1,4 @@
+function MyButton(){
+  return <button>Hey I'm Button</button>
+}
+export default MyButton;
